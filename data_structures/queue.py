b3dfed88cbf90f279cec0d73cba458afd6a01b7e
Tomer Avgil <132493674+tomeravgil@users.noreply.github.com>
2024-04-05 16:26:44 -0400

    Implemented grab multiple users

--- data_structures/queue.py
+++ data_structures/queue.py
@@ -1,4 +1,4 @@
-import User
+from data_structures import User
 
 class Queue:
     '''Queue class for drivers to use and handle users'''
@@ -21,21 +21,39 @@ class Queue:
             raise IndexError("Queue is empty")
         return self.queue.pop(0)
     
+    def see_next_user_people(self) -> 'int':
+        '''gets the number of people next user has without popping'''
+        return self.queue[0].get_num_people()
+
     # requires: len(queue) > 0
     # modifies: queue
+    # parameters: max people a driver can take
     # returns: list of User Objects with most efficient path
     #          keeping in mind driver spots
     # throws: Exception if queue is empty
-    def grab_multiple_users(self, val: int) -> list['User']:
+    def grab_multiple_users(self, max_people: int) -> list['User']:
         '''grabs multiple uses in the queue keeping in mind 
         an efficient path'''
+        driver_seats = max_people
+        people = []
+        while (driver_seats - self.see_next_user_people() > 0) :
+            user = self.grab_next_user()
+            people.append(user)
+            driver_seats -= user.get_num_people()
+        
+        efficient_path = graph_algorithms.get_efficient_path(people)
 
-    # requires: None
-    # modifies: None
-    # returns: queue length
-    # throws: None
+        return efficient_path
+    
+        
     def is_empty(self) -> int:
-        '''checks if queue is empty'''
+        '''
+        checks if queue is empty
+            @requires: None
+            modifies: None
+            returns: queue length
+            throws: None
+        '''
         return len(self.queue) == 0
     
     # requires: None
