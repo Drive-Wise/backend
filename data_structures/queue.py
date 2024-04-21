09d1ccdad3d60f3aa4c31f0540450e6c6531bbfa
Tomer Avgil <132493674+tomeravgil@users.noreply.github.com>
2024-04-20 21:30:12 -0400

    added specs to queue

--- data_structures/queue.py
+++ data_structures/queue.py
@@ -1,40 +1,104 @@
-from data_structures import User
+from models.user import User
 
 class Queue:
     '''Queue class for drivers to use and handle users'''
     def __init__(self):
         self.queue = []
 
-    # parameter: User object
-    # modifies: queue object
-    # return: None
-    def add(self, user: User) -> None:
-        '''adds user to queue'''
-        self.queue.append(user)
+    """    
+    parameter: User object
+    modifies: queue object
+    return: None
+    """
+    def add(self, user: 'User') -> None:
+        """
+        Adds user to queue and sorts queue
 
-    # requires: len(queue) > 0
-    # modifies: queue
-    # returns: first User Object in queue
-    # throws: Exception if queue is empty
+        Args:
+            User: user object
+
+        Returns:
+            self.is_driving: if driver is driving
+
+        Raises:
+            None
+        
+        Modifies: 
+            None
+        """
+        
+        self.queue.append(user)
+        self.queue.sort(key=lambda user: user.get_time())
+
     def grab_next_user(self) -> 'User':
-        '''gets the next user in the queue'''
+        """
+        gets the next user in the queue
+
+        Requires: 
+            len(queue) > 0
+
+        Args:
+            User: user object
+
+        Returns:
+            self.queue.pop(0): next user to pick up 
+
+        Raises:
+            IndexError if Queue is empty
+        
+        Modifies: 
+            queue: pops from queue next person
+        """
+
         if self.is_empty():
             raise IndexError("Queue is empty")
         return self.queue.pop(0)
     
     def see_next_user_people(self) -> 'int':
-        '''gets the number of people next user has without popping'''
+        """
+        peaks at next users number of people in queue without popping
+
+        Requires: 
+            len(queue) > 0
+
+        Args:
+            None
+
+        Returns:
+            self.queue[0].get_num_people(): number of people use has 
+            with them
+
+        Raises:
+            None
+        
+        Modifies: 
+            None
+        """
+
         return self.queue[0].get_num_people()
 
-    # requires: len(queue) > 0
-    # modifies: queue
-    # parameters: max people a driver can take
-    # returns: list of User Objects with most efficient path
-    #          keeping in mind driver spots
-    # throws: Exception if queue is empty
     def grab_multiple_users(self, max_people: int) -> list['User']:
-        '''grabs multiple uses in the queue keeping in mind 
-        an efficient path'''
+        """
+        grabs multiple uses in the queue keeping in mind 
+        an efficient path
+
+        Requires: 
+            len(queue) > 0
+
+        Args:
+            max people a driver can take
+            
+        Returns:
+            list of User Objects with most efficient path
+            keeping in mind driver spots
+
+        Raises:
+            Exception if queue is empty
+        
+        Modifies: 
+            queue
+        """
+
         driver_seats = max_people
         people = []
         while (driver_seats - self.see_next_user_people() > 0) :
@@ -48,13 +112,25 @@ class Queue:
     
         
     def is_empty(self) -> int:
-        '''
+        """
         checks if queue is empty
-            @requires: None
-            modifies: None
-            returns: queue length
-            throws: None
-        '''
+
+        Requires: 
+            None
+
+        Args:
+            None
+            
+        Returns:
+            boolean if queue is empty
+
+        Raises:
+           None
+        
+        Modifies: 
+            None
+        """
+
         return len(self.queue) == 0
     
     # requires: None
@@ -62,5 +138,23 @@ class Queue:
     # returns: queue length
     # throws: None
     def size(self) -> int:
-        '''checks queue size'''
+        """
+        checks queue size
+
+        Requires: 
+            None
+
+        Args:
+            None
+            
+        Returns:
+            queue length
+
+        Raises:
+           None
+        
+        Modifies: 
+            None
+        """
+
         return len(self.queue)
